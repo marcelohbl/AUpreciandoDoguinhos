e25d8d481e8964e57f597ff6d9c9952c94d0265e
marcelohbl <146591526+marcelohbl@users.noreply.github.com>
2023-09-30 21:14:54 -0300

    Create Cabecalho.js

---
+++ src/Componentes/Cabecalho.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export function Cabecalho() {
+  return (
+    <View style={estilos.container}>
+      <Text style={estilos.texto}>AUpreciando Doguinhos</Text>
+      <Text style={estilos.texto}>Descubra raças de cachorros fofinhos (ou não)</Text>
+    </View>
+  );
+}
+
+const estilos = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    color: '#fff', 
+    fontSize: 18,
+    fontFamily: 'Times New Roman', 
+  },
+});
